3368ba44bda66ec56698915234d58009eefbc3e2
guramchubinidze <guram.chubinidze@outlook.com>
2024-04-03 16:31:18 +0400

    Add options to massConverter

--- Module1_Python-Basics/massConvertor.py
+++ Module1_Python-Basics/massConvertor.py
@@ -2,5 +2,9 @@ unit = input("T or Kg? ")
 
 amount = float(input("Amount: "))
 
-
-print(f"{amount*1000} Kg")
+if unit =="T":
+    print(f"{amount*1000} Kg")
+elif  unit=="Kg":
+    print(f"{amount/1000} Kg")
+else:
+    print("Write correct Unit!")
